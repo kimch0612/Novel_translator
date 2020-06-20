1e48dafadf4cb5a931700da753d6af56c0fa97c7
kimch0612 <kimch061279@gmail.com>
2020-06-21 00:49:58 +0900

    First base coding

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 #Novel Translator via papago (JP -> KR)
+
+from requests import Request
+from requests import Session
+
+f = open("novel.txt", encoding='UTF8')
+text = f.read()
+f.close()
+
+s = Session()
+
+headers = {
+    'X-Naver-Client-Id': 'a',
+    'X-Naver-Client-Secret': 'a',
+}
+
+payload = {
+    'source': 'jp',
+    'target': 'ko',
+    'text': 'text'
+}
+
+url = 'https://openapi.naver.com/v1/language/translate'
+
+req = Request('POST', url, data=payload, headers=headers)
+prepped = req.prepare()
+
+res = s.send(prepped)
+
+result = res.json()['message']['result']['translatedText']
+
+print(result)
